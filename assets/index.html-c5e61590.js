4ff50b7f9efd0c2fef36846f74a9d5b24c62049b
Sanjeewa Wijesundara <sun@sanjeewa.org>
2022-12-17 08:01:47 +1100

    update site

---
+++ assets/index.html-c5e61590.js
@@ -0,0 +1,12 @@
+import{_ as i,M as s,p as c,q as r,N as a,V as t,R as e,t as n}from"./framework-8fa3e4ce.js";const d={},u=e("h3",{id:"as-easy-as-1-2-3",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#as-easy-as-1-2-3","aria-hidden":"true"},"#"),n(" As Easy as 1, 2, 3")],-1),p=e("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token comment"},"# install in your project"),n(`
+`),e("span",{class:"token function"},"pnpm"),n(),e("span",{class:"token function"},"add"),n(),e("span",{class:"token parameter variable"},"-D"),n(` vuepress@next @vuepress/client@next vue
+
+`),e("span",{class:"token comment"},"# create a markdown file"),n(`
+`),e("span",{class:"token builtin class-name"},"echo"),n(),e("span",{class:"token string"},"'# Hello VuePress'"),n(),e("span",{class:"token operator"},">"),n(` README.md
+
+`),e("span",{class:"token comment"},"# start writing"),n(`
+`),e("span",{class:"token function"},"pnpm"),n(` vuepress dev
+
+`),e("span",{class:"token comment"},"# build to static files"),n(`
+`),e("span",{class:"token function"},"pnpm"),n(` vuepress build
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),m=e("h3",{id:"how-to-vpn-with-gcp",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#how-to-vpn-with-gcp","aria-hidden":"true"},"#"),n(" How to VPN with GCP")],-1),h=e("p",null,"In this blog I'm going to work with gCP",-1);function v(_,b){const o=s("CodeGroupItem"),l=s("CodeGroup");return c(),r("div",null,[u,a(l,null,{default:t(()=>[a(o,{title:"PNPM",active:""},{default:t(()=>[p]),_:1})]),_:1}),m,h])}const k=i(d,[["render",v],["__file","index.html.vue"]]);export{k as default};
