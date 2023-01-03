849452659e8ab3f3a736426519b785da555bc1bd
Sanjeewa Wijesundara <sun@sanjeewa.org>
2023-01-03 11:05:51 +1100

    update

---
+++ assets/mermaid-mindmap.esm.min-947ff737.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-0b6c251e.js";import"./framework-767c0b94.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-73588523.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
